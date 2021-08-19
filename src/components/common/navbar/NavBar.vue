fdb0a73c7590a51965166e898da75b7e11e0510f
zhangliang <1617851696@qq.com>
2021-08-19 18:20:29 +0800

    网络请求部分

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+<!-- 因为每一页都有导航，所以这里就不适合设置 ID -->
+    <div class="nav-bar">
+    <!-- 为了方便为插槽布局，所以这里我们要用div将起包起来 ,因为有三个插槽，所以要使用name 便于区分-->
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar'
+}
+</script>
+
+<style>
+ .nav-bar {
+     display: flex;
+     /* 导航栏一般都是44px */
+     height: 44px;
+     line-height: 44px;
+     text-align: center;
+     box-shadow: 0 1px 1px rgba(100,100,100,.1);
+     
+ }
+ .left,.right {
+     width: 60px;
+ }
+ .center {
+     flex: 1;
+ }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-    <h2>我是首页</h2>
+    <div id="home">
+        <!-- 插槽替换的使用:现在需要用template来进行包装 -->
+        <!-- 这里为nav-bar设置class是为了在home组件中设置其样式 -->
+        <nav-bar class="home-nav">
+            <template v-slot:center>
+                <div>购物街</div>
+            </template>
+        </nav-bar>
+    </div>
 </template>
 
 <script>
+// 这里引用组件
+import NavBar from '@/components/common/navbar/NavBar.vue'
+
+// 因为导出的时候不是用default导出，所以导入要用{}
+import {getHomeMultidata} from "@/network/home"
 export default {
-    name:'Home'
+    name:'Home',
+    // 注册组件
+    components:{
+        NavBar
+    },
+    data() {
+        return {
+            result:null
+        }
+    },
+    // 在组件创建完成后马上发送网络请求
+    created() {
+        // 1.请求多个数据,使用.then就能返回数据
+        getHomeMultidata().then(res => {
+            console.log(res);
+            this.result = res
+        })
+    }
 }
 </script>
 
 <style>
-
+.home-nav {
+    /* 这里的颜色使用的的base.css文件中自己定义的颜色变量 */
+    background-color: var(--color-tint);
+    color: white;
+}
 </style>
